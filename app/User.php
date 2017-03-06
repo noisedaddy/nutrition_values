456e5e86a538cf378a9f115149237cf68a22e7cc
veljko <veljko.radenkovic@gmail.com>
2017-03-06 14:00:00 +0100

    test new WITH AUTHOR DATE

--- app/User.php
+++ app/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    /** User images test
+    /** User images this is another test
      * @return mixed
      */
     public function images(){

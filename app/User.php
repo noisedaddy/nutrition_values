d360d1c9b086a04127a96903b89baf8ee1131a02
veljko <veljko.radenkovic@gmail.com>
2017-03-13 15:53:30 +0100

    dsafdfds

--- app/User.php
+++ app/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    /** User images test
+    /** User images
      * @return mixed
      */
     public function images(){
